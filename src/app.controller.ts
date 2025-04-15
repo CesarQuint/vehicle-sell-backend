93c19f945eb795118655f19cf89819f8709e3f1c
CesarQuint <pedrocesarquintero001@gmail.com>
2025-04-14 21:22:55 -0600

    allow the upload of files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { AppService } from './app.service';
+import { PostVehicleDto } from './dto/post-vehicle.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('vehicle')
+  @UseInterceptors(FilesInterceptor('images', 3))
+  postVehicle(
+    @Body() dto: PostVehicleDto,
+    @UploadedFiles() images: Express.Multer.File[],
+  ) {
+    return this.appService.getHello(dto, images);
   }
 }
