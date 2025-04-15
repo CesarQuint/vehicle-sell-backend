c0bb32c9ab19942f1d4b66788d433c8ee4f82348
CesarQuint <pedrocesarquintero001@gmail.com>
2025-04-15 11:30:53 -0600

    made configuration to allow return of image file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, StreamableFile, NotFoundException } from '@nestjs/common';
+import { createReadStream, existsSync } from 'fs';
+import { join } from 'path';
 import { PuppeterService } from './services/puppeter.service';
 import { PostVehicleDto } from './dto/post-vehicle.dto';
+import { Response } from 'express';
 
 @Injectable()
 export class AppService {
   constructor(private readonly puppeterService: PuppeterService) {}
-  async getHello(
+  async postVehicle(
+    res: Response,
     postVehicleDto: PostVehicleDto,
     images: Express.Multer.File[],
   ) {
-    return await this.puppeterService.postVehicle(postVehicleDto, images);
+    try {
+      await this.puppeterService.postVehicle(postVehicleDto, images);
+
+      const filePath = join(process.cwd(), 'vehicle-post.png');
+
+      if (!existsSync(filePath)) {
+        throw new NotFoundException('Image not found.');
+      }
+
+      const fileStream = createReadStream(filePath);
+
+      res.set({
+        'Content-Type': 'image/png',
+        'Content-Disposition': 'inline; filename="vehicle-post.png"',
+      });
+
+      return new StreamableFile(fileStream);
+    } catch (error) {
+      throw error;
+    }
   }
 }
